ce78d64b3989a5cb715032e5c3cfc8bc2606c5e9
team11 <asd@asd.pl>
2015-01-26 22:21:13 +0100

    Added branch and bound algorithm

--- TSP_GA/src/Graph.java
+++ TSP_GA/src/Graph.java
@@ -1,3 +1,5 @@
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Random;
 import java.util.ArrayList;
 
@@ -36,6 +38,170 @@ public class Graph {
 		}
 	}
 	
+	public class BranchAndBound
+	{
+		private Graph graph_;
+		private int max_;
+		private ArrayList<ArrayList<Integer>> possibleNodes_;
+		private Integer FIRST_EDGE = 0;
+		
+		BranchAndBound(Graph graph){
+			graph_ = graph;
+			possibleNodes_ = new ArrayList<ArrayList<Integer>>(graph_.getSize());
+			
+			for(int i = 0; i <graph_.getSize(); ++i){
+				possibleNodes_.add(new ArrayList<Integer>(graph_.getSize()));
+				for(int j = 0; j < graph_.getSize();++j){
+					if(i != j){
+						possibleNodes_.get(i).add(j);
+					}
+				}
+			}
+			
+			for(int i = 0; i < possibleNodes_.size(); ++i){
+				Collections.sort(possibleNodes_.get(i), new NodeCostComparator(i, graph_));
+			}
+			
+			max_ = countMax();
+		}
+		
+		
+		public int findBestCost() {
+			ArrayList<Integer> path = new ArrayList<Integer>(graph_.getSize());
+			addNode(path, 0, possibleNodes_, 0, null,null);
+			return max_;
+		}
+		
+		private void addNode(ArrayList<Integer> path, Integer chosenNode, ArrayList<ArrayList<Integer>> possibleNodes, int cost, Integer firstNode, ArrayList<Integer> nodesToFirstEdge){
+			if(path.isEmpty()){
+				firstNode = chosenNode;
+				nodesToFirstEdge = initNodesToFirstEdge(firstNode);
+			}
+			else{
+				cost += graph_.getCost(path.get(path.size() - 1), chosenNode);
+				removeNodeFrom(possibleNodes, path.get(path.size() - 1), nodesToFirstEdge);
+				if(nodesToFirstEdge.isEmpty())
+				{
+					return;
+				}
+				removeNodeTo(possibleNodes, chosenNode);
+				if(minIsHigherThanMax(cost, possibleNodes)) return;
+				updateMax(cost, possibleNodes);
+			}
+			
+			path.add(chosenNode);
+			if(path.size() >= graph_.getSize()){
+				return;
+			}
+
+			
+			while(!possibleNodes.get(chosenNode).isEmpty()){
+				ArrayList<ArrayList<Integer>> newPossibleNodes = copyPossibleNodes(possibleNodes);
+				ArrayList<Integer> newPath = new ArrayList<Integer>(path);
+				ArrayList<Integer> newNodesToFirstEdge = new ArrayList<Integer>(nodesToFirstEdge);
+				Integer newChosenNode = possibleNodes.get(chosenNode).get(0);
+				if(newChosenNode.intValue() != firstNode.intValue()){
+					addNode(newPath, newChosenNode, newPossibleNodes, cost, firstNode, newNodesToFirstEdge);
+				}
+				if(chosenNode == firstNode.intValue()){
+					nodesToFirstEdge.remove(newChosenNode);
+				}
+				else if(newChosenNode == firstNode.intValue()){
+					nodesToFirstEdge.remove(chosenNode);
+				}
+				if(nodesToFirstEdge.isEmpty())
+				{
+					return;
+				}
+				possibleNodes.get(newChosenNode).remove(chosenNode);
+				possibleNodes.get(chosenNode).remove(newChosenNode);
+			}
+		}
+		
+		private ArrayList<Integer> initNodesToFirstEdge(Integer firstNode){
+			ArrayList<Integer> nodesToFirstEdge = new ArrayList<Integer>(graph_.getSize());
+			for(int i = 0; i<graph_.getSize(); ++i){
+				if(i != firstNode.intValue()){
+					nodesToFirstEdge.add(i);
+				}
+			}
+			return nodesToFirstEdge;
+		}
+		
+		private void removeNodeFrom(ArrayList<ArrayList<Integer>> possibleNodes, Integer from, ArrayList<Integer> nodesToFirstEdge){
+			possibleNodes.get(from).clear();
+			nodesToFirstEdge.remove(from);
+		}
+
+		private void removeNodeTo(ArrayList<ArrayList<Integer>> possibleNodes, Integer to){
+			for(ArrayList<Integer> a: possibleNodes){
+				a.remove(to);
+			}
+		}
+		
+		private ArrayList<ArrayList<Integer>> copyPossibleNodes(ArrayList<ArrayList<Integer>> possibleNodes){
+			ArrayList<ArrayList<Integer>> newPossibleNodes = new ArrayList<ArrayList<Integer>>(possibleNodes.size());
+			for(ArrayList<Integer> a: possibleNodes){
+				newPossibleNodes.add(new ArrayList<Integer>(a));
+			}
+			return newPossibleNodes;
+		}
+		
+		private boolean minIsHigherThanMax(int cost, ArrayList<ArrayList<Integer>> possibleNodes){
+			int min = cost;
+			for(int i = 0; i < possibleNodes.size(); ++i){
+				ArrayList<Integer> a = possibleNodes.get(i);
+				if(a.isEmpty()){
+					continue;
+				}
+				min += graph_.getCost(i, a.get(0));
+			}
+			return min > max_;
+		}
+		
+		private int countMax() {
+			int max = 0;
+			for(int i = 0; i < possibleNodes_.size(); ++i){
+				ArrayList<Integer> a = possibleNodes_.get(i);
+				if(a.isEmpty()){
+					continue;
+				}
+				max += graph_.getCost(i, a.get(a.size() - 1));
+			}
+			return max;
+		}
+		
+		private void updateMax(int cost, ArrayList<ArrayList<Integer>> possibleNodes) {
+			int max = cost;
+			for(int i = 0; i < possibleNodes.size(); ++i){
+				ArrayList<Integer> a = possibleNodes.get(i);
+				if(a.isEmpty()){
+					continue;
+				}
+				max += graph_.getCost(i, a.get(a.size() - 1));
+			}
+			max_ = Math.min(max, max_);
+		}
+		
+		private class NodeCostComparator implements Comparator<Integer> {
+		    private Graph graph_;
+		    private Integer firstEdge_;
+		    
+		    public NodeCostComparator(Integer edge, Graph graph){
+		    	graph_ = graph;
+		    	firstEdge_ = edge;
+		    }
+		    
+			@Override
+		    public int compare(Integer i1, Integer i2) {
+				if (i1 == i2){
+					return 0;
+				}
+				return graph_.getCost(firstEdge_, i1) < graph_.getCost(firstEdge_, i2) ? -1 : 1;
+		    }
+		}
+	}
+	
 	public int findBestPathCost(){
 		ArrayList<Integer> path = new ArrayList<Integer>();
 		ArrayList<Integer> unvisited = new ArrayList<Integer>();
@@ -44,6 +210,11 @@ public class Graph {
 		}
 		path.add(0);
 		return countBest(path, unvisited);
+	}
+	
+	public int findBestPathCost2(){
+		BranchAndBound b = new BranchAndBound(this);
+		return b.findBestCost();
 	}
 	
 	private int countBest(ArrayList<Integer> path, ArrayList<Integer> unvisited){
